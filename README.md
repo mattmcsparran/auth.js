af266808aee645bf04e94be70ab1d67b22bda426
Matthew McSparran <mattmcsparran@gmail.com>
2017-11-18 12:36:14 -0500

    fix
    fixed issues with login, added node_modules to .gitignore, updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # auth.js
 A simple authentication and authorization module for building APIs when using front-end frameworks
+
+## Getting Started
+
+Installing via npm:
+```
+npm i --save auth.js
+```
+
+Adding the module to your project:
+
+```
+const auth = require('auth.js');
+const authConfig = require('auth.js/lib/config');
+```
+
+## Configuring the variables
+The configuration function is as follows
+```
+authConfig.conf(secret, saltRounds, expire);
+```
+There only two options that have defaults are ```saltRounds``` and ```expire```, which are ```16``` and ```{ expiresIn: '1h' }``` respectively.
+
+The recommended configuration is to use ```dotenv``` to place the JWT secret and salt rounds in the ```.env``` file as seen in the example below.
+
+```
+authConfig.conf(process.env.SECRET_JWT, process.env.SALTROUNDS, { expiresIn: '4h' });
+```
+Auth.js uses [jsonwebtoken](https://github.com/auth0/node-jsonwebtoken) for tokens and follows auth0's formatting.
+
+## Configuring auth.js with mongoose
+Require it in your user schema like so:
+
+```
+const mongoose = require("mongoose");
+const auth = require('auth.js');
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+UserSchema.plugin(auth)
+module.exports = mongoose.model("User", UserSchema);
+```
+
+## Using auth.js in your routes
+Require it in your route.
+
+Here is a example of the registration method:
+
+```
+service.register = (req, res) => {
+  const newUser = new User({username: req.body.username, email: req.body.email});
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      res.status(err.code).send(err.body)
+    }
+    res.status(200).json(user);
+  });
+  ```
+## Error handling
+In the last example, you can see that auth.js handles its own errors and other messages. Messages are stored in a message object:
+
+  ```
+messages = {
+  body: '',
+  type: '',
+  code: ''
+}
+```
+The module will use pre determined error messages and codes, and will send what type of message it is (currently: success or error) which will also allow you to utilize alerts on your front end.
--- index.js
+++ index.js
@@ -3,21 +3,18 @@
  * Copyright(c) 2017 Matthew McSparran
  * MIT Licensed
  */
-
 // Module Dependencies
-
 const _ = require('lodash');
 const mongoose = require('mongoose');
 const debug = require('debug');
 // Module Scripts
 const config = require('./lib/config');
 const b = require('./lib/bcrypt');
-const jwt = require('./lib/jwt');
+const j = require('./lib/jwt');
 const messages = require('./lib/messages');
 const errors = require('./lib/errors');
 const success = require('./lib/success')
 // Configuration
-
 setMessages = (body, type, code) => {
   messages.body = body;
   messages.type = type;
@@ -31,11 +28,9 @@ module.exports = function(schema, options) {
   options.findByUsername = options.findByUsername || function(model, queryParameters) { return model.findOne(queryParameters); }
   options.passwordField = options.passwordField || 'password';
   options.errorMessages = options.errorMessages || {};
-  options.saltRounds = config.saltRounds();
-  options.tokenExpire = config.tokenExpire();
-  options.bcryptSecret = config.bcryptSecret();
-  options.jwtSecret = config.jwtSecret();
-
+  options.saltRounds = function() { return config.saltRounds || 16 } ;
+  options.tokenExpire = function() { return config.tokenExpire || { expiresIn: '1h' } };
+  options.jwtSecret = function() { return config.jwtSecret };
   options.passwordValidator = options.passwordValidator || function(password, callback) { callback(null); };
   if (options.usernameQueryFields) {
     options.usernameQueryFields.push(options.usernameField);
@@ -55,77 +50,48 @@ schema.pre('save', function(next) {
       next();
     });
 schema.methods.setPassword = function (password, callback) {
-    console.log('checking for password');
   if (!password) {
     return callback(setMessages(errors.missingPassword, 'error', 406));
   }
   let self = this;
   options.passwordValidator(password, (err) => {
-    console.log(password);
-    console.log('validating password');
     if (err) {
     return callback(err); }
-    b.salt(password, options.saltRounds, (err, salt) => {
-      console.log('hashing password');
+    b.salt(password, options.saltRounds(), (err, salt) => {
       if (err) { console.log(err);
         return callback(err); }
-      console.log(salt);
       self.set(options.passwordField, salt);
       callback(null, self);
     });
   });
 };
-schema.methods.comparePassword = (password, callback) => {
+schema.methods.comparePassword = function (password, callback) {
   let self = this;
-  b.validate(password, self.password, (err, validated) => {
-    if(!validated || err) {
+  b.validate(password, self.password, function(err, validated) {
+    if(!validated) {
       return callback(setMessages(errors.incorrectPassword, 'error', 401));
     }
-      return callback(validated);
-  });
-
-};
-schema.methods.changePassword = function(oldPassword, newPassword, callback) {
-  if (!oldPassword || !newPassword) {
-    return callback(new errors.MissingPasswordError(options.errorMessages.MissingPasswordError));
-  }
-
-  var self = this;
-
-  this.authenticate(oldPassword, function(err, authenticated) {
-    if (err) { return callback(err); }
-
-    if (!authenticated) {
-      return callback(new errors.IncorrectPasswordError(options.errorMessages.IncorrectPasswordError));
-    }
-
-    self.setPassword(newPassword, function(setPasswordErr, user) {
-      if (setPasswordErr) { return callback(setPasswordErr); }
-
-      self.save(function(saveErr) {
-        if (saveErr) { return callback(saveErr); }
-
-        callback(null, user);
-      });
-    });
+      return callback(null, validated);
   });
 };
-schema.statics.login = (user, password, callback) => {
+schema.statics.login = function (user, password, callback) {
   let self = this;
-    self.findByUsername(user, (err, foundUser) => {
+    self.findByUsername(user, function (err, foundUser) {
       if (err) { callback(setMessages(errors.incorrectUsername, 'error', 401)); }
       if (foundUser) {
-        self.comparePassword(password, (error, validated) => {
+        foundUser.comparePassword(password, function (error, validated) {
           if (!validated) {return callback(setMessages(errors.incorrectPassword, 'error', 401)); }
-          jwt.issueToken(foundUser, config.jwtSecret, config.tokenExpire, (err, token) => {
+          j.issueToken(foundUser, options.jwtSecret(), options.tokenExpire(), (err, token) => {
+            console.log(token);
             if (err) { callback(setMessages(errors.unknown, 'error', 400)); }
+            console.log(foundUser);
             callback(null, token);
           });
         });
       }
     });
-}
-schema.statics.register = function (user, password, callback) {
+  };
+  schema.statics.register = function (user, password, callback) {
     if (!(user instanceof this)) {
         user = new this(user);
       }
@@ -149,39 +115,22 @@ schema.statics.register = function (user, password, callback) {
         });
       });
     });
-  }
+  };
   schema.statics.findByUsername = function(username, selectPasswordField, callback) {
     if (typeof callback === 'undefined') {
       callback = selectPasswordField;
       selectPasswordField = false;
     }
-
-    // if specified, convert the username to lowercase
-    if (username !== undefined && options.usernameLowerCase) {
+    if (username !== undefined) {
       username = username.toLowerCase();
     }
-
-    // Add each username query field
     var queryOrParameters = [];
     for (var i = 0; i < options.usernameQueryFields.length; i++) {
       var parameter = {};
       parameter[options.usernameQueryFields[i]] = username;
       queryOrParameters.push(parameter);
     }
-
     var query = options.findByUsername(this, { $or: queryOrParameters });
-
-    if (selectPasswordField) {
-      query.select('+' + options.passwordField);
-    }
-
-    if (options.selectFields) {
-      query.select(options.selectFields);
-    }
-
-    if (options.populateFields) {
-      query.populate(options.populateFields);
-    }
 
     if (callback) {
       query.exec(callback);
@@ -189,4 +138,4 @@ schema.statics.register = function (user, password, callback) {
       return query;
     }
   };
-}
+};
